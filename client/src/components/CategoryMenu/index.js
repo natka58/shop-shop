9d3ab1b8e505807f343c29009757accfa32a693c
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-14 16:34:23 -0700

    errors

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,49 +1,62 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
-import { useStoreContext } from "../../utils/GlobalState";
-import { useEffect } from 'react';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-function CategoryMenu( ) {
-  const [state, dispatch] = useStoreContext();
+// import { useStoreContext } from "../../utils/GlobalState";
+import { idbPromise } from '../../utils/helpers';
+import { useSelector, useDispatch } from "react-redux";
 
-const { categories } = state;
+function CategoryMenu() {
+    // const [state, dispatch] = useStoreContext();
+    const state = useSelector(state => state);
 
-const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+    const dispatch = useDispatch();
 
-// receive as a prop from the Home component
-useEffect(() => {
-  // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-  if (categoryData) {
-    // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
-    dispatch({
-      type: UPDATE_CATEGORIES,
-      categories: categoryData.categories
-    });
-  }
-}, [categoryData, dispatch]);
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-const handleClick = id => {
-  dispatch({
-    type: UPDATE_CURRENT_CATEGORY,
-    currentCategory: id
-  });
-};
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map(item => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+    const { categories } = state;
+
+    useEffect(() => {
+        if (categoryData) {
+            dispatch({
+                type: UPDATE_CATEGORIES,
+                categories: categoryData.categories
+            });
+            categoryData.categories.forEach(category => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then(categories => {
+                dispatch({
+                    type: UPDATE_CATEGORIES,
+                    categories: categories
+                });
+            });
+        }
+    }, [categoryData, loading, dispatch]);
+
+    const handleClick = id => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id
+        });
+    };
+
+    return (
+        <div>
+            <h2>Choose a Category:</h2>
+            {categories.map(item => (
+                <button
+                    key={item._id}
+                    onClick={() => {
+                        handleClick(item._id);
+                    }}
+                >
+                    {item.name}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 export default CategoryMenu;
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,28 +1,24 @@
 import React, { createContext, useContext } from "react";
 import { useProductReducer } from './reducers';
 
-
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-
-
-
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: ''
     });
     // use this to confirm it works!
     console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
-  };
+};
 
-  
-  const useStoreContext = () => {
+const useStoreContext = () => {
     return useContext(StoreContext);
-  };
+};
 
-
-  export { StoreProvider, useStoreContext };
+export { StoreProvider, useStoreContext };
